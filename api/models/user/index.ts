eadf337adbe8b12ccf3a6d0df7f5ae4d581f4203
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-08-21 11:48:53 +0100

    chore(api/model): update user model to allow onboarding details

--- api/models/user/index.ts
+++ api/models/user/index.ts
@@ -1,6 +1,12 @@
 import { Types } from "mongoose";
 import { prop, getModelForClass, modelOptions } from "@typegoose/typegoose";
 
+export enum USER_TYPE {
+  CREATOR = "creator",
+  FREELANCER = "freelancer",
+  NOT_SPECIFIED = "not_specified",
+}
+
 @modelOptions({
   schemaOptions: {
     timestamps: true,
@@ -22,6 +28,16 @@ class User {
   @prop({ required: true })
   name: string;
 
+  @prop({ required: false, default: "" })
+  displayName: string;
+
+  @prop({ required: false, default: USER_TYPE.NOT_SPECIFIED })
+  userType: USER_TYPE;
+
+  @prop({ required: false, default: [] })
+  specialities: string[]; /** The domain the user works in (e.g. "software development", "design", "marketing", etc.) */
+
+  @prop({ required: false })
   @prop({ required: true, unique: true })
   email: string;
 
